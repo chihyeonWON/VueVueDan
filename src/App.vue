0c991f55fbc3ef26202700e2c593339ec7a1a8df
원치현 <heungeob1003@gmail.com>
2021-10-30 20:21:56 +0900

    If user correct, print new first, second number, '정답' else print '오답'

--- src/App.vue
+++ src/App.vue
@@ -3,10 +3,13 @@
     <div>
       {{ first }} 곱하기 {{ second }} 는?
     </div>
-    <form>
+    <form v-on:submit="onSubmitForm">
       <input type="number" v-model="inputValue">
       <button type="submit">입력</button>
     </form>
+    <div id="result">
+      {{result}}
+    </div>
   </div>
 </template>
 
@@ -19,8 +22,24 @@ export default {
       first:Math.ceil(Math.random()*9),
       second:Math.ceil(Math.random()*9),
       inputValue:'',
+      result:'',  
     }
-  }
+  },
+  methods: {
+    onSubmitForm(e) {
+      e.preventDefault(); // 버튼 눌렀을 때 새로고침되는 것을 방지
+      // 첫번째 숫자와 두번째 숫자를 곱한 값이 입력한 숫자와 같을 때 정답을 출력 그렇지 않으면 땡을 출력
+      if(this.first * this.second === parseInt(this.inputValue)) {
+        this.result='정답';
+        this.first = Math.ceil(Math.random() * 9);
+        this.second = Math.ceil(Math.random() * 9);
+        this.inputValue = '';
+      } else {
+        this.result='땡';
+        this.inputValue='';
+      }
+    },
+  },
 }
 </script>
 
